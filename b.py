736cf882d2078643e339a0d702841053faf8dfa3
arjunsuwal <arjunsuwal900@gmail.com>
2021-09-10 04:35:57 +0545

    feri arko commit garera haleko hai aba herau k hunxa

---
+++ b.py
@@ -0,0 +1,6 @@
+print("what is my name you wanna show it")
+print("how can i help you ")
+x= 900
+if x == 900:
+     print("congrats you have printed the value of x right")
+     print('la check garta aba difference')
